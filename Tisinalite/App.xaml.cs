138b27eb791ecabfee5a4a3eb09be9ee8724a90e
Maxim Terlyck <zaraz121801@gmail.com>
2022-05-03 12:07:52 +0300

    Реализация создания папки приложения при запуске

--- Tisinalite/App.xaml.cs
+++ Tisinalite/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using System.Diagnostics;
 
 namespace Tisinalite
 {
@@ -13,5 +15,21 @@ namespace Tisinalite
     /// </summary>
     public partial class App : Application
     {
+        public string NotesDir;
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                //Debug.WriteLine( "\n\n" + Directory.Exists(Environment.GetEnvironmentVariable("USERPROFILE") + @"\Documents"));
+                NotesDir = Environment.GetEnvironmentVariable("USERPROFILE") + @"\Documents\Tisinalite";
+                if (!Directory.Exists(NotesDir))
+                {
+                    Debug.WriteLine("А где");
+                    Directory.CreateDirectory(NotesDir);
+                    string f = "Welcome.txt";
+                    File.Copy(Path.Combine("Resources", f), Path.Combine(NotesDir, f));
+                }
+            }
+        }
     }
 }
