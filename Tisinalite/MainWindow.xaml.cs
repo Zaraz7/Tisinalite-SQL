579dcf368c8f31d5ef865afea121c5d6403a7cb2
Maxim Terlyck <zaraz121801@gmail.com>
2022-05-07 12:07:32 +0300

    Оптимизация кода

--- Tisinalite/MainWindow.xaml.cs
+++ Tisinalite/MainWindow.xaml.cs
@@ -11,8 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Diagnostics;
+using System.IO;
+
 
 namespace Tisinalite
 {
@@ -21,17 +22,143 @@ namespace Tisinalite
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Settings _settings = Settings.GetSettings();
+        private string notePath;
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Pages.Editor());
+            if (_settings.OpenNote != null)
+            {
+                notePath = Global.GetNoteFile(_settings.OpenNote);
+                OpenFile(_settings.OpenNote);
+            }
+            UpdateTreeView();
+
+        }
+        public void SaveSettings()
+        {
+            _settings.Save();
+        }
+
+        private void NewExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (notePath != null)
+                SaveFile();
+            notePath = null;
+            tbEditor.Text = "";
+            SaveFile();
+            UpdateTreeView();
+        }
+        private void SaveExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFile();
+            SaveSettings();
+            UpdateTreeView();
+        }
+        private void DeleteExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (MessageBox.Show("Уверены, что хоте удалить заметку?", "Подтверждение действия",
+                MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                File.Delete(notePath);
+                notePath = null;
+                UpdateTreeView();
+                tbEditor.Clear();
+                _settings.OpenNote = null;
+            }
+        }
+        private void CloseExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveExecute(sender, e);
+            Application.Current.Shutdown();
+        }
+        private void UpdateTreeView()
+        {
+            tvNotes.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(Global.NotesDir);
+            tvNotes.Items.Add(CreateDirNode(rootDirectoryInfo));
+        }
+        private static TreeViewItem CreateDirNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Items.Add(new TreeViewItem { Header = file.Name });
+
+            return directoryNode;
+        }
+
+
+        public void SaveFile()
+        {
+            if (notePath == null && !String.IsNullOrEmpty(tbEditor.Text))
+            {
+                Pages.Input input = new Pages.Input();
+                input.Title = "Введите имя фала";
+                input.ShowDialog();
+                try
+                {
+                    string path = Path.Combine(Global.NotesDir, input.Entry);
+
+                    if (!input.Result) return;
+                    notePath = path;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+            if (notePath != null)
+            {
+                using (StreamWriter writer = new StreamWriter(notePath))
+                {
+                    writer.WriteLine(tbEditor.Text);
+                    writer.Close();
+                    Markdownview.Markdown = tbEditor.Text;
+                }
+            }
+        }
+        private void OpenFile(string note)
+        {
+            string _notePath = Global.GetNoteFile(note);
+            if (notePath != null && _notePath != notePath)
+                SaveFile();
+            try
+            {
+                using (StreamReader reader = new StreamReader(_notePath))
+                {
+                    string noteText = reader.ReadToEnd();
+                    tbEditor.Text = noteText;
+                    reader.Close();
+                    notePath = _notePath;
+                    _settings.OpenNote = note;
+                    Markdownview.Markdown = noteText;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void tvNote_Changed(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeViewItem item = (TreeViewItem)tvNotes.SelectedItem;
+            Debug.WriteLine(item);
+            if (item != null)
+            {
+                string note = item.Header.ToString();
+                OpenFile(note);
+            }
+        }
+
+        private void CanDeleteExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = File.Exists(notePath);
         }
 
         private void ClosingEvent(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Pages.Editor editor = MainFrame.Content as Pages.Editor;
-            editor.SaveFile();
-            editor.SaveSettings();
+            SaveFile();
+            SaveSettings();
         }
     }
 }
