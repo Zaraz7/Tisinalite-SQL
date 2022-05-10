d390f1477eac5e02d6de66d0f2b0db094e19fede
Maxim Terlyck <zaraz121801@gmail.com>
2022-05-10 12:37:53 +0300

    Удаление мертвого кода

--- Tisinalite/Pages/General.xaml.cs
+++ Tisinalite/Pages/General.xaml.cs
@@ -27,6 +27,7 @@ namespace Tisinalite.Pages
         private Settings _settings = Settings.GetSettings();
         //private string notePath;
         Users user;
+        Notes openNote;
         public General(Users _user)
         {
             user = _user;
@@ -80,7 +81,7 @@ namespace Tisinalite.Pages
             tvNotes.Items.Clear();
             var userOfGroups = TisinaliteDBEntities.GetContext().UsersOfGroups.ToList();
             var groups = TisinaliteDBEntities.GetContext().UsersOfGroups.ToList();
-            
+
             userOfGroups = userOfGroups.Where(g => g.UserID == user.ID).ToList();
 
 
@@ -88,110 +89,50 @@ namespace Tisinalite.Pages
             {
                 using (var db = new TisinaliteDBEntities())
                 {
-                    var group = db.Groups.AsNoTracking().FirstOrDefault(g=> g.ID == link.GroupID);
-                    var groupNode = new TreeViewItem { Header = group.Title };
-                    foreach (var note in TisinaliteDBEntities.GetContext().Notes.Where(n => n.GroupID == group.ID).ToList())
+                    var group = db.Groups.AsNoTracking().FirstOrDefault(g => g.ID == link.GroupID);
+                    var groupNode = new TreeViewItem { Header = group.Title, Tag = group };
+
+                    foreach (var note in TisinaliteDBEntities.GetContext().Notes
+                        .Where(n => n.GroupID == group.ID).ToList())
                     {
-                        var noteNode = new TreeViewItem { Header = note.Title, Foreground = Brushes.White};
+                        var noteNode = new TreeViewItem { Header = note.Title, Foreground = Brushes.White, Tag = note };
                         groupNode.Items.Add(noteNode);
                     }
                     tvNotes.Items.Add(groupNode);
                 }
-
             }
-            /*
-            Debug.WriteLine("Searching groups");
-            var groups = TisinaliteDBEntities.GetContext().Groups.ToList();
-            groups = groups.Where(g => g.UsersOfGroups == user.UsersOfGroups).ToList();
-            Debug.WriteLine(groups.Count());
-            
-            var mainNode = new TreeViewItem();
-            foreach (var group in groups)
-            {
-                Debug.WriteLine(group.Title);
-                var groupNode = new TreeViewItem { Header = group.Title };
-                mainNode.Items.Add(groupNode);
-            }
-            tvNotes.Items.Add(mainNode);*/
-            /* ------------------ */
-            //tvNotes.Items.Clear();
-            //var rootDirectoryInfo = new DirectoryInfo(Global.NotesDir);
-            //tvNotes.Items.Add(CreateDirNode(rootDirectoryInfo));
         }
-        //private static TreeViewItem CreateDirNode(DirectoryInfo directoryInfo)
-        //{
-        //    var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
-        //    foreach (var file in directoryInfo.GetFiles())
-        //        directoryNode.Items.Add(new TreeViewItem { Header = file.Name });
 
-        //    return directoryNode;
-        //}
 
         public void NewFile()
         {
-            //Pages.Input input = new Pages.Input
-            //{
-            //    Title = "Введите имя фала"
-            //};
-            //input.ShowDialog();
-            //try
-            //{
-            //    string path = Path.Combine(Global.NotesDir, input.Entry);
 
-            //    if (!input.Result) return;
-            //    File.Create(path);
-            //    notePath = path;
-            //}
-            //catch (Exception e)
-            //{
-            //    MessageBox.Show(e.ToString());
-            //}
         }
         public void SaveFile()
         {
-            //if (notePath == null)
-            //{
-            //    NewFile();
-            //}
-            //if (notePath != null)
-            //{
-            //    using (StreamWriter writer = new StreamWriter(notePath))
-            //    {
-            //        writer.WriteLine(tbEditor.Text);
-            //        writer.Close();
-            //    }
-            //}
+
         }
-        private void OpenFile(string note)
+        private void OpenNote()
         {
-            //string _notePath = Global.GetNoteFile(note);
-            //if (notePath != null && _notePath != notePath)
-            //    SaveFile();
-            //try
-            //{
-            //    using (StreamReader reader = new StreamReader(_notePath))
-            //    {
-            //        string noteText = reader.ReadToEnd();
-            //        tbEditor.Text = noteText;
-            //        reader.Close();
-            //        notePath = _notePath;
-            //        _settings.OpenNote = note;
-            //    }
-            //}
-            //catch
-            //{
-            //}
+
         }
 
         private void tvNote_Changed(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            //TreeViewItem item = (TreeViewItem)tvNotes.SelectedItem;
-            //Debug.WriteLine(item);
-            //if (item != null)
-            //{
-            //    string note = item.Header.ToString();
-            //    OpenFile(note);
-            //}
+            TreeViewItem item = (TreeViewItem)tvNotes.SelectedItem;
+            if (item.Foreground.Equals(Brushes.White))
+            {
+                Debug.WriteLine("Note");
+                Notes note = item.Tag as Notes;
+                Debug.WriteLine(note.Title);
+            }
+            else
+            {
+                Debug.WriteLine("Group");
+                Groups group = item.Tag as Groups;
+                Debug.WriteLine(group.Title);
+            }
+
         }
 
         private void CanDeleteExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -201,8 +142,7 @@ namespace Tisinalite.Pages
 
         private void ClosingEvent(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            //SaveFile();
-            //SaveSettings();
+
         }
 
         private void PasteExecute(object sender, ExecutedRoutedEventArgs e)
@@ -212,20 +152,7 @@ namespace Tisinalite.Pages
 
         private void Image_Click(object sender, RoutedEventArgs e)
         {
-            //var fileContent = string.Empty;
-            //var filePath = string.Empty;
 
-            //OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            //openFileDialog.Filter = "Файлы изображений(*.PNG;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF;*.PNG|Все файлы (*.*)|*.*";
-            //openFileDialog.FilterIndex = 1;
-            //openFileDialog.RestoreDirectory = true;
-
-            //if (openFileDialog.ShowDialog() == true)
-            //{
-            //    filePath = openFileDialog.FileName;
-            //    tbEditor.SelectedText = $" ![Название картинки]({filePath}) ";
-            //}
         }
     }
 }
