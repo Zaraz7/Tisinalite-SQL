c107c722ad4b1df01a3ecf544ca44173706ed038
Maxim Terlyck <zaraz121801@gmail.com>
2022-06-13 18:09:04 +0300

    Добавление библиотеки по проверки пароля

---
+++ ClassLibraryPassword/PasswordTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPassword
+{
+    public class PasswordTest
+    {
+        // Проверка на сложность и доступность пароля
+        public static bool PasswordHardCorrect(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            // Если нет этих символов проверка также проваливается
+            if (password.Intersect("#$%_").Count() == 0)
+                return false;
+            return true;
+        }
+    }
+}
